6c0269d1858b72eb91b1a40ddc66f4877ff04c14
Jiexi Cao <jiexicao@gmail.com>
2014-11-15 04:44:17 -0800

    group page pulls data correctly for the most part and renders it yay

--- engauge.py
+++ engauge.py
@@ -2,10 +2,11 @@
 import json
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
-import firebase
+from firebase import firebase
 
 app = Flask(__name__)
 
+FIREBASE_URL = 'https://engauge.firebaseio.com/'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -13,10 +14,24 @@ PASSWORD = 'default'
 
 app.config.from_object(__name__)
 
+engauge_fb = firebase.FirebaseApplication(FIREBASE_URL)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    result = engauge_fb.get('/groups', None)
+    return render_template('index.html', result=result)
+
+@app.route('/group/<groupId>')
+def group(groupId):
+    groupInfo = engauge_fb.get('/groups/'+groupId, None) 
+    qids = engauge_fb.get('/groups/'+groupId+'/questions', None)
+    questions = []    
+
+    for q in qids:
+        questions.append(engauge_fb.get('/groups/'+groupId+'/questions/'+q, None))
+
+    return render_template('group.html', groupInfo=groupInfo, questions=questions)
+
 
 if __name__ == '__main__':
     app.run()
